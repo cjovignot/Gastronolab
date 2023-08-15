8e67107d3d5c6adc49714e89bf5c2839f451bd3a
cjovignot <cosme.jovignot@epitech.eu>
2023-08-15 10:52:12 +0200

    [WIP] carousel home beverage page

---
+++ app/components/HealthyBeverages/Home/welcome.jsx
@@ -0,0 +1,25 @@
+const healthyBeverageWelcome = () => {
+
+    return (
+        <div className="hero max-h-[700px] bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div class="gallery">
+                    <img src="https://images.unsplash.com/photo-1483918793747-5adbf82956c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="a house on a mountain" />
+                    <img src="https://images.unsplash.com/photo-1414450397866-85f90db48714?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="sime pink flowers" />
+                    <img src="https://images.unsplash.com/photo-1464593745417-e22a78ca277b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80" alt="big rocks with some trees" />
+                    <img src="https://images.unsplash.com/photo-1443131307017-4097c8ac7763?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=756&q=80" alt="a waterfall, a lot of tree and a great view from the sky" />
+                    <img src="https://images.unsplash.com/photo-1514995669114-6081e934b693?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="a cool landscape" />
+                    <img src="https://images.unsplash.com/photo-1502741224143-90386d7f8c82?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80" alt="inside a town between two big buildings" />
+                    <img src="https://images.unsplash.com/photo-1528498033373-3c6c08e93d79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=685&q=80" alt="a great view of the sea above the mountain" />
+                </div>
+                <div>
+                    <h1 className="text-5xl font-bold">Healthy Beverages</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-outline rounded-full border-black text-black hover:bg-red-800">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default healthyBeverageWelcome;
--- app/pages/reciepes/healthyBeverages/page.jsx
+++ app/pages/reciepes/healthyBeverages/page.jsx
@@ -1,10 +1,7 @@
+import HealthyBeverageWelcome from '../../../components/HealthyBeverages/Home/welcome'
+
 export default function HealthyBeverages() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-            <h1>This is my Healthy Beverages page</h1>
-
-
-        </main>
+        <HealthyBeverageWelcome />
     )
 }
